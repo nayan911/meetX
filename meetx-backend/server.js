bc877cce29b9d27e7007b8bc6fad43a8eb5d045d
nayan911 <nayanagarwal911@gmail.com>
2025-05-10 20:13:51 +0530

    added vercel.json

--- meetx-backend/server.js
+++ meetx-backend/server.js
@@ -15,7 +15,11 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://deploy-mern-1whq.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 
 app.use('/api/auth', auth);
 app.use('/api/activities', activities);
